5f54316f83effa41dc207f6f0da2d0ceb093c712
r4s_4l_3oul 49 <anassmoumen92290@gmail.com>
2026-03-16 18:13:35 +0100

    Update 1_creation.sql

--- 1_creation.sql
+++ 1_creation.sql
@@ -19,23 +19,6 @@ CREATE TABLE Categorie(
    PRIMARY KEY(C_ID)
 );
 
-CREATE TABLE Commentaire(
-   Com_ID INT,
-   Com_commentaire VARCHAR(50),
-   Com_date DATE,
-   U_ID INT NOT NULL,
-   PRIMARY KEY(Com_ID),
-   FOREIGN KEY(U_ID) REFERENCES Utilisateur(U_ID)
-);
-
-CREATE TABLE favori(
-   F_ID INT,
-   F_date DATE,
-   U_ID INT NOT NULL,
-   PRIMARY KEY(F_ID),
-   FOREIGN KEY(U_ID) REFERENCES Utilisateur(U_ID)
-);
-
 CREATE TABLE Bibliotheque(
    B_id INT,
    B_ville VARCHAR(50),
@@ -47,6 +30,27 @@ CREATE TABLE Bibliotheque(
    PRIMARY KEY(B_id)
 );
 
+CREATE TABLE Commentaire(
+   Com_ID INT,
+   Com_commentaire VARCHAR(50),
+   Com_date DATE,
+   U_ID INT,
+   PRIMARY KEY(Com_ID),
+   FOREIGN KEY (U_ID) REFERENCES Utilisateur(U_ID)
+      ON DELETE SET NULL
+      ON UPDATE CASCADE
+);
+
+CREATE TABLE favori(
+   F_ID INT,
+   F_date DATE,
+   U_ID INT,
+   PRIMARY KEY(F_ID),
+   FOREIGN KEY (U_ID) REFERENCES Utilisateur(U_ID)
+      ON DELETE SET NULL
+      ON UPDATE CASCADE
+);
+
 CREATE TABLE Livre(
    L_ID INT,
    L_titre VARCHAR(50),
@@ -55,32 +59,46 @@ CREATE TABLE Livre(
    L_langue VARCHAR(50),
    L_status VARCHAR(50),
    L_auteur VARCHAR(50),
-   U_ID INT NOT NULL,
-   B_id INT NOT NULL,
+   U_ID INT,
+   B_id INT,
    PRIMARY KEY(L_ID),
-   FOREIGN KEY(U_ID) REFERENCES Utilisateur(U_ID),
-   FOREIGN KEY(B_id) REFERENCES Bibliotheque(B_id)
+   FOREIGN KEY (U_ID) REFERENCES Utilisateur(U_ID)
+      ON DELETE SET NULL
+      ON UPDATE CASCADE,
+   FOREIGN KEY (B_id) REFERENCES Bibliotheque(B_id)
+      ON DELETE SET NULL
+      ON UPDATE CASCADE
 );
 
 CREATE TABLE Note(
    N_ID INT,
    N_valeur INT,
    N_date DATE,
-   L_ID INT NOT NULL,
-   Com_ID INT NOT NULL,
-   U_ID INT NOT NULL,
+   L_ID INT,
+   Com_ID INT,
+   U_ID INT,
    PRIMARY KEY(N_ID),
-   FOREIGN KEY(L_ID) REFERENCES Livre(L_ID),
-   FOREIGN KEY(Com_ID) REFERENCES Commentaire(Com_ID),
-   FOREIGN KEY(U_ID) REFERENCES Utilisateur(U_ID)
+   FOREIGN KEY (L_ID) REFERENCES Livre(L_ID)
+      ON DELETE SET NULL
+      ON UPDATE CASCADE,
+   FOREIGN KEY (Com_ID) REFERENCES Commentaire(Com_ID)
+      ON DELETE SET NULL
+      ON UPDATE CASCADE,
+   FOREIGN KEY (U_ID) REFERENCES Utilisateur(U_ID)
+      ON DELETE SET NULL
+      ON UPDATE CASCADE
 );
 
 CREATE TABLE classer(
    L_ID INT,
    C_ID INT,
    PRIMARY KEY(L_ID, C_ID),
-   FOREIGN KEY(L_ID) REFERENCES Livre(L_ID),
-   FOREIGN KEY(C_ID) REFERENCES Categorie(C_ID)
+   FOREIGN KEY (L_ID) REFERENCES Livre(L_ID)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+   FOREIGN KEY (C_ID) REFERENCES Categorie(C_ID)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
 );
 
 CREATE TABLE follow(
@@ -88,6 +106,10 @@ CREATE TABLE follow(
    U_ID_1 INT,
    follow_date DATE,
    PRIMARY KEY(U_ID, U_ID_1),
-   FOREIGN KEY(U_ID) REFERENCES Utilisateur(U_ID),
-   FOREIGN KEY(U_ID_1) REFERENCES Utilisateur(U_ID)
+   FOREIGN KEY (U_ID) REFERENCES Utilisateur(U_ID)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE,
+   FOREIGN KEY (U_ID_1) REFERENCES Utilisateur(U_ID)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
 );
